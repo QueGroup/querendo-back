a341b491319977907ba0352a7209d5b3a1a16f3d
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2022-12-29 18:52:13 +0300

    🚧 Add a test

--- src/profiles/tests.py
+++ src/profiles/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+import os
 
-# Create your tests here.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+import django
+
+django.setup()
+from rest_framework.test import APITestCase
+
+
+class UserAPITestCase(APITestCase):
+    def test_get(self):
+        pass
