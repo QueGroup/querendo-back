9f3bc30dea15535f6194616b8d40f8b84286c6a6
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2023-01-27 20:01:22 +0300

    ♻️ Refactor code

--- config/settings.py
+++ config/settings.py
@@ -5,9 +5,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-t_02+wqa(^(ktsevoc!fswi&%u+=&exu7+)+ewem^e99!nw+v!'
 
@@ -29,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'djoser',
+    'cities_light',
     'drf_yasg',
     "corsheaders",
     'src.profiles',
