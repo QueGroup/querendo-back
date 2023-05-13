143c823a0b63e6089b2146e0fdcd1d5414be9092
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2023-05-13 14:35:09 +0300

    🔥Remove files

--- src/likes/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from src.likes.models import Likes
-
-
-class LikeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Likes
-        fields = ('id',
-                  'liked')
