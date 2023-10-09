279a10c41d022da2b5083e77533ecf5a3ec95bbc
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2023-10-09 23:21:42 +0300

    🎨Middleware displays response time

--- middlewares/middleware.py
+++ middlewares/middleware.py
@@ -1,3 +1,5 @@
+import time
+
 import structlog
 
 logger = structlog.get_logger()
@@ -8,12 +10,23 @@ class LoggingMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Логирование перед обработкой запроса
         logger.info('Processing request', request_path=request.path)
 
-        response = self.get_response(request)
+        start_time = time.time()
 
-        # Логирование после обработки запроса
-        logger.info('Processed request', request_path=request.path, response_status=response.status_code)
+        try:
+            response = self.get_response(request)
+        except Exception as e:
+            logger.error('An error occurred', request_path=request.path, error=str(e))
+            raise
+
+        response_time = int((time.time() - start_time) * 1000)
+
+        logger.info(
+            'Processed request',
+            request_path=request.path,
+            response_status=response.status_code,
+            response_time=response_time
+        )
 
         return response
